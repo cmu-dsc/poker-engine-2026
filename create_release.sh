691f25d3bc3b8f44ad0623ae5b7b9400604737ef
clementou <clement.h.ou@gmail.com>
2025-03-20 22:51:04 -0400

    include docs in release

--- create_release.sh
+++ create_release.sh
@@ -9,13 +9,14 @@ zip_name="aipoker-${current_date}.zip"
 # Create temporary folder
 temp_dir="aipoker"
 rm -rf "$temp_dir"
-mkdir -p "$temp_dir/agents" "$temp_dir/submission"
+mkdir -p "$temp_dir/agents" "$temp_dir/submission" "$temp_dir/docs"
 
 # Copy files and folders to temporary directory, maintaining structure
 cp gym_env.py match.py run.py agent_test.py requirements.txt "$temp_dir/"
 cp agents/agent.py "$temp_dir/agents/"
 cp agents/test_agents.py "$temp_dir/agents/"
 cp -r submission/* "$temp_dir/submission/"
+cp -r docs/* "$temp_dir/docs/"
 
 # Create zip file while excluding unwanted files
 zip -r "$zip_name" "$temp_dir" \
